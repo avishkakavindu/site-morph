0be25f46dda5a1a020682f79ee3ca2006e21b12a
econ-dev-avishka <avishka@econsulate.net>
2025-01-23 16:14:03 +0530

    Simple script added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 var $ = require('jquery');
 var _ = require('lodash');
 
-// TODO:: script
+(function () {
+  var $searchInput = $('#search-wrapper input[name="s"]');
+
+  $searchInput.on('input', function () {
+    console.log('Search input changed: ', $(this).val());
+    if ($(this).val().length > 0) {
+      alert('You have typed: ' + $(this).val());
+    }
+  });
+  alert('Event listener added to search input');
+})();
